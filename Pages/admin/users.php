ff2c2f477ab8737a0849bae42ee9b374d11a7b12
KhyeeHEN <henkhyee4.0@gmail.com>
2025-05-30 16:22:18 +0800

    Update users.php

--- Pages/admin/users.php
+++ Pages/admin/users.php
@@ -74,15 +74,15 @@ $error = isset($_GET['error']) ? $_GET['error'] : null;
             <!-- Display User Data -->
             <div class="table-container <?php echo (!isset($_GET['active_tab']) || $_GET['active_tab'] === 'students') ? 'active' : ''; ?>"
                 id="students-table">
-                <?php include '/Pages/sql/students_data.php'; ?>
+                <?php include 'Pages/sql/students_data.php'; ?>
             </div>
             <div class="table-container <?php echo (isset($_GET['active_tab']) && $_GET['active_tab'] === 'instructors') ? 'active' : ''; ?>"
                 id="instructors-table">
-                <?php include '/Pages/sql/instructors_data.php'; ?>
+                <?php include 'Pages/sql/instructors_data.php'; ?>
             </div>
             <div class="table-container <?php echo (isset($_GET['active_tab']) && $_GET['active_tab'] === 'admins') ? 'active' : ''; ?>"
                 id="admins-table">
-                <?php include '/sql/admins_data.php'; ?>
+                <?php include 'Pages/sql/admins_data.php'; ?>
             </div>
         </main>
     </div>
