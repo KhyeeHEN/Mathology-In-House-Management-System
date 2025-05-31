0bacbfcdbfcb64f3bc04aa11cf86ce88d203612d
KhyeeHEN <henkhyee4.0@gmail.com>
2025-05-31 13:30:18 +0800

    Update timetable_approve.php

--- Pages/admin/timetable_approve.php
+++ Pages/admin/timetable_approve.php
@@ -330,8 +330,8 @@ if (!$viewing_id && !$show_search_results) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Timetable Approval</title>
-    <link rel="stylesheet" href="../../styles/common.css">
-    <link rel="stylesheet" href="../../styles/timtable.css">
+    <link rel="stylesheet" href="/Styles/common.css">
+    <link rel="stylesheet" href="/Styles/timtable.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
     <style>
         .tab-container { display: flex; margin-bottom: 20px; }
