aafd1b0e438d7d76ed6201a82de0cf4def7f5059
KhyeeHEN <henkhyee4.0@gmail.com>
2025-06-01 19:01:35 +0800

    css for action buttons

--- sql/students_data.php
+++ sql/students_data.php
@@ -91,9 +91,15 @@ if ($result->num_rows > 0) {
             <td>" . ($row['Gender'] ? 'Male' : 'Female') . "</td>
             <td>" . $row['email'] . "</td>
             <td>
-                <button onclick=\"toggleDetails('$detailsId')\">View Details</button>
-                <a href='../../sql/edit_student.php?student_id={$row['student_id']}'>Edit</a>
-                <a href='../../sql/delete_student.php?student_id={$row['student_id']}' onclick=\"return confirm('Are you sure you want to delete this student?');\">Delete</a>
+                <button class='action-btn view' onclick=\"toggleDetails('$detailsId')\">View Details</button>
+                <form method='get' action='../../sql/edit_student.php' style='display:inline; margin:0; padding:0;'>
+                    <input type='hidden' name='student_id' value='{$row['student_id']}'>
+                    <button type='submit' class='action-btn edit'>Edit</button>
+                </form>
+                <form method='get' action='../../sql/delete_student.php' style='display:inline; margin:0; padding:0;' onsubmit=\"return confirm('Are you sure you want to delete this student?');\">
+                    <input type='hidden' name='student_id' value='{$row['student_id']}'>
+                    <button type='submit' class='action-btn delete'>Delete</button>
+                </form>
             </td>
           </tr>";
 
