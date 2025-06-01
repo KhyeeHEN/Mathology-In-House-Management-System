cd0b32fa80ff3b954536e83ff1688a7d4fbe113c
KhyeeHEN <henkhyee4.0@gmail.com>
2025-06-01 19:04:59 +0800

    Update instructors_data.php

--- sql/instructors_data.php
+++ sql/instructors_data.php
@@ -91,12 +91,12 @@ if ($result->num_rows > 0) {
             <td>" . $row['email'] . "</td>
             <td>
                 <button class='action-btn view' onclick=\"toggleDetails('$detailsId')\">View Details</button>
-                <form method='get' action='../../sql/edit_student.php' style='display:inline; margin:0; padding:0;'>
-                    <input type='hidden' name='student_id' value='{$row['student_id']}'>
+                <form method='get' action='../../sql/edit_instructor.php' style='display:inline; margin:0; padding:0;'>
+                    <input type='hidden' name='instructor_id' value='{$row['instructor_id']}'>
                     <button type='submit' class='action-btn edit'>Edit</button>
                 </form>
-                <form method='get' action='../../sql/delete_student.php' style='display:inline; margin:0; padding:0;' onsubmit=\"return confirm('Are you sure you want to delete this student?');\">
-                    <input type='hidden' name='student_id' value='{$row['student_id']}'>
+                <form method='get' action='../../sql/delete_instructor.php' style='display:inline; margin:0; padding:0;' onsubmit=\"return confirm('Are you sure you want to delete this student?');\">
+                    <input type='hidden' name='instructor_id' value='{$row['instructor_id']}'>
                     <button type='submit' class='action-btn delete'>Delete</button>
                 </form>
             </td>
