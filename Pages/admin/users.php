5ea60264cbde7ab1a2e30c75c13a7872f0d40210
KhyeeHEN <henkhyee4.0@gmail.com>
2025-06-01 18:23:54 +0800

    Update users.php

--- Pages/admin/users.php
+++ Pages/admin/users.php
@@ -14,7 +14,7 @@ $error = isset($_GET['error']) ? $_GET['error'] : null;
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Users</title>
     <link rel="stylesheet" href="/Styles/common.css">
-    <link rel="stylesheet" href="/Styles/users.css?ver=<?php echo time(); ?>">
+    <link rel="stylesheet" href="/Styles/users.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
     <script type="module" src="/Scripts/common.js"></script>
     <script src="/Scripts/users.js?v=<?php echo time(); ?>"></script>
