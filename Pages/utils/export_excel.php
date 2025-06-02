a46f8916674f49a797e7c9477f4fa5c11dff5db8
Jeff <wjn9038@gmail.com>
2025-06-03 00:37:55 +0800

    Update export report

--- Pages/utils/export_excel.php
+++ Pages/utils/export_excel.php
@@ -1,5 +1,7 @@
 <?php
-require '../vendor/autoload.php';
+// require '../vendor/autoload.php';
+require '../../vendor/autoload.php';
+
 
 use PhpOffice\PhpSpreadsheet\Spreadsheet;
 use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
