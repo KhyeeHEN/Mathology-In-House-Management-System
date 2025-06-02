18ed33dd79e27dabd9c00d809b9dee79e52535d9
Darrshan <129675691+Darrshan23@users.noreply.github.com>
2025-06-02 20:52:33 +0800

    Update dashboardAdmin2.php

--- Pages/admin/dashboardAdmin2.php
+++ Pages/admin/dashboardAdmin2.php
@@ -95,8 +95,7 @@ $attendanceData = $attendanceResult->fetch_assoc();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin Dashboard</title> 
-    <!-- <link rel="stylesheet" href="/Styles/dashboard.css"> -->
+    <title>Admin Dashboard</title>
     <link rel="stylesheet" href="/Styles/common.css">
     <link rel="stylesheet" href="/Styles/dashboardAdmin2.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
