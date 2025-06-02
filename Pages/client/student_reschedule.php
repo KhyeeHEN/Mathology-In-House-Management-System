5ea65155273328748ec33efcffb2a37682b20d49
Cylim1507 <121530482+Cylim1507@users.noreply.github.com>
2025-06-02 16:53:18 +0800

    Update student_reschedule.php

--- Pages/client/student_reschedule.php
+++ Pages/client/student_reschedule.php
@@ -91,8 +91,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['request_reschedule'])
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Request Timetable Change</title>
-    <link rel="stylesheet" href="../styles/common.css">
-    <link rel="stylesheet" href="../styles/attendence.css">
+    <link rel="stylesheet" href="../../Styles/common.css">
+    <link rel="stylesheet" href="../../Styles/attendence.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
     <style>
         .content-container {
