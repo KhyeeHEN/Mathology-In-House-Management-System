59bd4ad7e50e453951aab17177d2c789d93ea701
Jeff <wjn9038@gmail.com>
2025-06-17 02:30:03 +0800

    payment testing

--- sql/process_student_payment.php
+++ sql/process_student_payment.php
@@ -3,17 +3,16 @@ session_start();
 require_once $_SERVER['DOCUMENT_ROOT'] . '/Pages/setting.php';
 
 $student_id = $_POST['student_id'];
-$course_id = $_POST['course_id'];
 $mode = $_POST['payment_mode'];
 $method = $_POST['payment_method'];
 $amount = $_POST['payment_amount'];
 $is_new = $_POST['is_new'];
 
 // Insert payment
-$stmt = $conn->prepare("INSERT INTO payment (student_id, course_id, payment_method, payment_mode, payment_amount, deposit_status, payment_status)
+$stmt = $conn->prepare("INSERT INTO payment (student_id, payment_method, payment_mode, payment_amount, deposit_status, payment_status)
                         VALUES (?, ?, ?, ?, ?, ?, 'pending')");
 $deposit_status = $is_new ? 'yes' : 'no';
-$stmt->bind_param("iissds", $student_id, $course_id, $method, $mode, $amount, $deposit_status);
+$stmt->bind_param("iissds", $student_id, $method, $mode, $amount, $deposit_status);
 $stmt->execute();
 $stmt->close();
 
