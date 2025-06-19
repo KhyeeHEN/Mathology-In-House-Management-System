ee53a9a4b5563548ae2b82db92d9f6fc16729dbe
KhyeeHEN <henkhyee4.0@gmail.com>
2025-06-19 17:43:03 +0800

    Update students_data.php

--- sql/students_data.php
+++ sql/students_data.php
@@ -107,14 +107,20 @@ if ($result->num_rows > 0) {
             <td>" . ($row['Gender'] ? 'Male' : 'Female') . "</td>
             <td>" . $row['email'] . "</td>
             <td>
-                <button class='action-btn view' onclick=\"toggleDetails('$detailsId')\">View Details</button>
+                <button class='action-btn view' onclick=\"toggleDetails('{$detailsId}')\">
+                    <i class='fas fa-eye'></i> View Details
+                </button>
                 <form method='get' action='../../sql/edit_student.php' style='display:inline; margin:0; padding:0;'>
                     <input type='hidden' name='student_id' value='{$row['student_id']}'>
-                    <button type='submit' class='action-btn edit'>Edit</button>
+                    <button type='submit' class='action-btn edit'>
+                        <i class='fas fa-edit'></i> Edit
+                    </button>
                 </form>
                 <form method='get' action='../../sql/delete_student.php' style='display:inline; margin:0; padding:0;' onsubmit=\"return confirm('Are you sure you want to delete this student?');\">
                     <input type='hidden' name='student_id' value='{$row['student_id']}'>
-                    <button type='submit' class='action-btn delete'>Delete</button>
+                    <button type='submit' class='action-btn delete'>
+                        <i class='fas fa-trash'></i> Delete
+                    </button>
                 </form>
             </td>
           </tr>";
