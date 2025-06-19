919354353d6ea8df541bc30d4c638ef2d72decca
KhyeeHEN <henkhyee4.0@gmail.com>
2025-06-19 21:36:07 +0800

    Update Aside_Nav_Instructor.php

--- Pages/includes/Aside_Nav_Instructor.php
+++ Pages/includes/Aside_Nav_Instructor.php
@@ -17,7 +17,7 @@
         </a>
                 <a href="../instructors/attendance_instructors.php"
             class="nav-item <?php echo basename($_SERVER['PHP_SELF']) == 'attendance_instructors.php' ? 'active' : ''; ?>">
-            <i class="fas fa-check"></i>
+            <i class="fas fa-calendar-alt"></i>
             <span>Attendance</span>
         </a>
     </nav>
